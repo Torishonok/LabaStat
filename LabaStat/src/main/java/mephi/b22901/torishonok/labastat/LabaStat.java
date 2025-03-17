caa2f95ea2fb38333c0fce980411ed35316a4c54
Torishonok <vikushonok2004@gmail.com>
2025-03-17 16:32:19 +0300

    Creature

---
+++ LabaStat/src/main/java/mephi/b22901/torishonok/labastat/LabaStat.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package mephi.b22901.torishonok.labastat;
+
+/**
+ *
+ * @author vikus
+ */
+public class LabaStat {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
